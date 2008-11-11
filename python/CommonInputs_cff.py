50697dfda22f65a49dbe64b88b49c37811ca1e50
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2008-11-11 13:07:51 +0000

    After correcting the tracker misalignment, the bad muon chambers need to be re-introduced

--- python/CommonInputs_cff.py
+++ python/CommonInputs_cff.py
@@ -15,6 +15,7 @@ GlobalTrajectoryBuilderCommon.TrackTransformer.TrackerRecHitBuilder = 'WithoutRe
 
 # The condDB setup (the global tag refers to DevDB, IntDB or ProDB whenever needed)
 from Configuration.StandardSequences.FrontierConditions_GlobalTag_cfi import *
+from CalibMuon.CSCCalibration.CSC_BadChambers_cfi import *
 hcal_db_producer = cms.ESProducer("HcalDbProducer",
     dump = cms.untracked.vstring(''),
     file = cms.untracked.string('')
