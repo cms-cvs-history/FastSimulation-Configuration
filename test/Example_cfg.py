3404f610b38f48840845200cbd877e78ced99973
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2008-11-11 11:21:26 +0000

    Ooops - revert to previous version (done for electrons and misalignment)

--- test/Example_cfg.py
+++ test/Example_cfg.py
@@ -16,7 +16,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 process.load("FastSimulation/Configuration/RandomServiceInitialization_cff")
 
 # Generate H -> ZZ -> l+l- l'+l'- (l,l'=e or mu), with mH=180GeV/c2
-#process.load("FastSimulation/Configuration/HZZllll_cfi")
+process.load("FastSimulation/Configuration/HZZllll_cfi")
 # Generate ttbar events
 #  process.load("FastSimulation/Configuration/ttbar_cfi")
 # Generate multijet events with different ptHAT bins
@@ -28,10 +28,10 @@ process.load("FastSimulation/Configuration/RandomServiceInitialization_cff")
 # process.load("FastSimulation/Configuration/FlatPtMuonGun_cfi")
 # replace FlatRandomPtGunSource.PGunParameters.PartID={130}
 # Generate di-electrons with pT=35 GeV
-process.load("FastSimulation/Configuration/DiElectrons_cfi")
+# process.load("FastSimulation/Configuration/DiElectrons_cfi")
 
 # Famos sequences (Frontier conditions)
-process.load("FastSimulation/Configuration/CommonInputs_cff")
+process.load("FastSimulation/Configuration/CommonInputsFake_cff")
 process.load("FastSimulation/Configuration/FamosSequences_cff")
 
 # Parametrized magnetic field (new mapping, 4.0 and 3.8T)
@@ -40,21 +40,14 @@ process.load("Configuration.StandardSequences.MagneticField_38T_cff")
 process.VolumeBasedMagneticFieldESProducer.useParametrizedTrackerField = True
 
 # If you want to turn on/off pile-up
-process.famosPileUp.PileUpSimulator.averageNumber = 0.0    
+process.famosPileUp.PileUpSimulator.averageNumber = 5.0    
 # You may not want to simulate everything for your study
 process.famosSimHits.SimulateCalorimetry = True
 process.famosSimHits.SimulateTracking = True
-process.famosSimHits.MaterialEffects.Bremsstrahlung = True
 # process.famosSimHits.SimulateMuons = False
 
-# Apply Tracker misalignment
-process.GlobalTag.globaltag = "STARTUP_V5::All"
-process.famosSimHits.ApplyAlignment = True
-process.misalignedTrackerGeometry.applyAlignment = True
-
-
 # Produce Tracks and Clusters
-process.p1 = cms.Path(process.famosWithElectrons)
+process.p1 = cms.Path(process.famosWithTracksAndEcalClusters)
 
 # To write out events (not need: FastSimulation _is_ fast!)
 process.o1 = cms.OutputModule(
@@ -64,12 +57,12 @@ process.o1 = cms.OutputModule(
                                            "drop *_mix_*_*")
     )
 
-# process.outpath = cms.EndPath(process.o1)
+process.outpath = cms.EndPath(process.o1)
 
 # Keep the logging output to a nice level #
 # process.Timing =  cms.Service("Timing")
-process.load("FWCore/MessageService/MessageLogger_cfi")
-process.MessageLogger.destinations = cms.untracked.vstring("pyDetailedInfo.txt")
+# process.load("FWCore/MessageService/MessageLogger_cfi")
+# process.MessageLogger.destinations = cms.untracked.vstring("pyDetailedInfo.txt")
 
 # Make the job crash in case of missing product
 process.options = cms.untracked.PSet( Rethrow = cms.untracked.vstring('ProductNotFound') )
