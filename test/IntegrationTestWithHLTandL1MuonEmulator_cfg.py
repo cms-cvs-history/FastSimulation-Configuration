d64a6e106d8c6746905e8a3a3bbf5407ec0f64de
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2009-07-16 14:50:15 +0000

    syncing with V01-10-15 HLTrigger/Configuration - /dev/CMSSW_3_1_0/pre11/1E31_V12/V2 updating global tags in tets configs to MC_31X_V3 and STARTUP31X_V2

--- test/IntegrationTestWithHLTandL1MuonEmulator_cfg.py
+++ test/IntegrationTestWithHLTandL1MuonEmulator_cfg.py
@@ -72,8 +72,8 @@ process.VolumeBasedMagneticFieldESProducer.useParametrizedTrackerField = True
 process.famosPileUp.PileUpSimulator.averageNumber = 0.0
 
 # Get frontier conditions   - not applied in the HCAL, see below
-# Values for globaltag are "STARTUP31X_V1::All","MC_31X_V1::All"
-process.GlobalTag.globaltag = "STARTUP31X_V1::All"
+# Values for globaltag are "STARTUP31X_V2::All","MC_31X_V3::All"
+process.GlobalTag.globaltag = "STARTUP31X_V2::All"
 
 
 # Apply ECAL and HCAL miscalibration 
@@ -88,7 +88,7 @@ process.misalignedTrackerGeometry.applyAlignment = True
 process.misalignedDTGeometry.applyAlignment = True
 process.misalignedCSCGeometry.applyAlignment = True
 
-# Attention ! for the HCAL IDEAL==STARTUP
+# Attention ! for the HCAL MC==STARTUP
 # process.caloRecHits.RecHitsFactory.HCAL.Refactor = 1.0
 # process.caloRecHits.RecHitsFactory.HCAL.Refactor_mean = 1.0
 # process.caloRecHits.RecHitsFactory.HCAL.fileNameHcal = "hcalmiscalib_0.0.xml"
