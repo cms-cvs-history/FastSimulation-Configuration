43f28806392510e71c3f3e4deac21195fb528b67
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2009-07-24 12:13:25 +0000

    - afer switching to OI a full-hltL3Trajectory module is required. - getting hltL3TkTracksFromL2 from confDB. - HLT ConfDB change to /dev/CMSSW_3_2_0/pre1/XXX_V20/V2 - Global Tags update in test configs to MC_31X_V3 and STARTUP31X_V2

--- test/ExampleWithHLT_cfg.py
+++ test/ExampleWithHLT_cfg.py
@@ -44,7 +44,7 @@ process.load("Configuration.Generator.TTbar_cfi")
 
 # Common inputs, with fake conditions
 process.load("FastSimulation.Configuration.CommonInputs_cff")
-process.GlobalTag.globaltag = "STARTUP31X_V1::All"
+process.GlobalTag.globaltag = "STARTUP31X_V2::All"
 
 # L1 Emulator and HLT Setup
 process.load("FastSimulation.HighLevelTrigger.HLTSetup_cff")
