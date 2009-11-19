8dcb4e3f4594cf8fff0c54295857f121ce246af3
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2009-11-19 09:19:26 +0000

    Global tags update to V14 version for 34X

--- test/ExampleWithHLT_1E31_cfg.py
+++ test/ExampleWithHLT_1E31_cfg.py
@@ -44,7 +44,7 @@ process.load("Configuration.Generator.TTbar_cfi")
 
 # Common inputs, with fake conditions
 process.load("FastSimulation.Configuration.CommonInputs_cff")
-process.GlobalTag.globaltag = "MC_3XY_V13::All"
+process.GlobalTag.globaltag = "MC_3XY_V14::All"
 
 # L1 Emulator and HLT Setup
 process.load("FastSimulation.HighLevelTrigger.HLTSetup_cff")
