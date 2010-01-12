6b7f9163409dcf6a2e527649e6e30958b49500cf
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2010-01-12 14:01:33 +0000

    Updating configs to GlobalTag V15 and followint  V01-13-56 HLTrigger/Configuration - ConfDB /dev/CMSSW_3_4_0/XXXX/V1

--- python/blockHLT_1E31_cff.py
+++ python/blockHLT_1E31_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_3_4_0/pre9/1E31/V4 (CMSSW_3_4_0_pre7_HLT1)
+# /dev/CMSSW_3_4_0/1E31/V1 (CMSSW_3_4_1)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_3_4_0/pre9/1E31/V4')
+  tableName = cms.string('/dev/CMSSW_3_4_0/1E31/V1')
 )
 
 
--- python/blockHLT_8E29_cff.py
+++ python/blockHLT_8E29_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_3_4_0/pre9/8E29/V4 (CMSSW_3_4_0_pre7_HLT1)
+# /dev/CMSSW_3_4_0/8E29/V1 (CMSSW_3_4_1)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_3_4_0/pre9/8E29/V4')
+  tableName = cms.string('/dev/CMSSW_3_4_0/8E29/V1')
 )
 
 
