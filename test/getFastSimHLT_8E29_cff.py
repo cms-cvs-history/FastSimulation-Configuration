70f263f3b1b3265039362ea95bb915419189d8f6
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2010-04-23 12:57:46 +0000

    Put back the HLT_MinBiasEcal path in the FastSim menu

--- test/getFastSimHLT_8E29_cff.py
+++ test/getFastSimHLT_8E29_cff.py
@@ -330,7 +330,6 @@ else:
         paths += "-AlCa_EcalPi0_8E29,"
         paths += "-AlCa_EcalEta_8E29,"
 ###AP *** other paths removed with V01-16-23 HLTrigger/Configuration - ConfDB /dev/CMSSW_3_5_5/XXXX/V21
-        paths += "-HLT_MinBiasEcal,"
         paths += "-HLT_DoublePhoton4_eeRes_L1R,"
         paths += "-HLT_DoublePhoton4_Jpsi_L1R,"
         paths += "-HLT_DoublePhoton4_Upsilon_L1R,"
