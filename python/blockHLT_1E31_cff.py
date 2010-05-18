2bf106e67887f85cb75d859b36c7108ec7ed3c0d
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2010-05-18 15:48:04 +0000

    for 360patch1 to cope with V01-17-52 HLTrigger/Configuration

--- python/blockHLT_1E31_cff.py
+++ python/blockHLT_1E31_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_3_6_0/1E31/V18 (CMSSW_3_6_0_HLT6)
+# /dev/CMSSW_3_6_0/1E31/V30 (CMSSW_3_6_0_HLT9)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_3_6_0/1E31/V18')
+  tableName = cms.string('/dev/CMSSW_3_6_0/1E31/V30')
 )
 
 
