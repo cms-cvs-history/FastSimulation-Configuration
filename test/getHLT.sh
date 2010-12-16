bd936ecde2f278c8c342ef834d014cf9621496ed
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2010-12-16 11:17:15 +0000

    remove unnecessary FastSimulation.Configuration.blockHLT_GRun_cff

--- test/getHLT.sh
+++ test/getHLT.sh
@@ -2,10 +2,9 @@
 
 # clean
 rm -f $CMSSW_BASE/src/FastSimulation/Configuration/python/HLT_{8E29,1E31,GRun}_cff.py
-rm -f $CMSSW_BASE/src/FastSimulation/Configuration/python/blockHLT_{8E29,1E31,GRun}_cff.py
 
 echo "Dumping GRun configuration"
-./getFastSimHLT_GRun_cff.py $(head -1 HLTVersionGRun) "../python/HLT_GRun_cff.py" "../python/blockHLT_GRun_cff.py"
+./getFastSimHLT_GRun_cff.py $(head -1 HLTVersionGRun) "../python/HLT_GRun_cff.py"
 
 #echo "Dumping 8E29 configuration"
 #./getFastSimHLT_8E29_cff.py $(head -1 HLTVersion8E29) "../python/HLT_8E29_cff.py" "../python/blockHLT_8E29_cff.py"
