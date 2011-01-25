e35c4b19ea4d7cec27c6c99d947621d383ee0122
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2011-01-25 23:02:54 +0000

    extract fastsim configuration using hltGetConfiguration

--- test/getHLT.sh
+++ test/getHLT.sh
@@ -1,13 +1,4 @@
 #! /bin/bash
 
-# clean
-rm -f $CMSSW_BASE/src/FastSimulation/Configuration/python/HLT_{8E29,1E31,GRun}_cff.py
-
 echo "Dumping GRun configuration"
-./getFastSimHLT_GRun_cff.py $(head -1 HLTVersionGRun) "../python/HLT_GRun_cff.py"
-
-#echo "Dumping 8E29 configuration"
-#./getFastSimHLT_8E29_cff.py $(head -1 HLTVersion8E29) "../python/HLT_8E29_cff.py" "../python/blockHLT_8E29_cff.py"
-
-#echo "Dumping 1E31 configuration"
-#./getFastSimHLT_1E31_cff.py $(head -1 HLTVersion1E31) "../python/HLT_1E31_cff.py" "../python/blockHLT_1E31_cff.py"
+hltGetConfiguration $(head -1 HLTVersionGRun) --fastsim > $CMSSW_BASE/src/FastSimulation/Configuration/python/HLT_GRun_cff.py
