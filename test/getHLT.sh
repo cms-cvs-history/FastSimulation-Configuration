a43c08cca67b3a6dfa2c01892fa342cb0491097d
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2011-09-25 22:53:16 +0000

    print configuration name before dumping it

--- test/getHLT.sh
+++ test/getHLT.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-echo "Dumping GRun configuration"
+HLT=$(head -1 HLTVersionGRun)
+echo "Dumping HLT configuration: $HLT"
+
 # The FastSim configuration is managed through HLTrigger/Configuration/python/Tools/confdb.py,
 # which is where the hltGetConfiguration command actually points to
-hltGetConfiguration $(head -1 HLTVersionGRun) --fastsim > $CMSSW_BASE/src/FastSimulation/Configuration/python/HLT_GRun_cff.py
+hltGetConfiguration $HLT --fastsim > $CMSSW_BASE/src/FastSimulation/Configuration/python/HLT_GRun_cff.py
