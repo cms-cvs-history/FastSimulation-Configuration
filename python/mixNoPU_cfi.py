58cd896e43dec8fcd7752a0406f4b8552116bbda
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-06-01 18:01:46 +0000

    Initial changes for pileup memory reduction

--- python/mixNoPU_cfi.py
+++ python/mixNoPU_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from FastSimulation.Configuration.mixFastSimObjects_cfi import *
 mix = cms.EDProducer("MixingModule",
-    digitizers = cms.VPSet(),
+    digitizers = cms.PSet(),
     LabelPlayback = cms.string(''),
     maxBunch = cms.int32(0),
     minBunch = cms.int32(0),
